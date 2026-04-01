d574a7b385ac8f0cd7e4ec28e1ff4e279b48f140
Muntasir <alimuntasir2001@gmail.com>
2026-04-02 00:00:57 +0600

    Python learning 1

--- new.py
+++ new.py
@@ -1,3 +1,4 @@
-name = "Muntasir"
-print(name)
+name = "Muntasir" 
+Message= "Hello Muntasir, welcome to the world of GenAI development!"
+print(name,Message)
 
---
+++ upper.py
@@ -0,0 +1,8 @@
+name="Muntasir"
+message="Hello, my name is Muntasir! I am learning Python programming to become a GenAI developer.  I am excited to explore the world of artificial intelligence and machine learning. I believe that with dedication and hard work, I can achieve my goal of becoming a successful GenAI developer. I am looking forward to the journey ahead and the opportunities it will bring! "
+upper_Case=message.upper()
+
+# print(upper_Case)
+
+print(name.title())
+
