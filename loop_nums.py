31261f33973b1f8235733369e53e1baa3d718570
Muntasir <alimuntasir2001@gmail.com>
2026-04-02 21:36:33 +0600

    Python learning 5

--- loop_nums.py
+++ loop_nums.py
@@ -3,8 +3,15 @@ list_nums = []
 
 for value in range(1,10):
     list_nums.append(value)
-    value=value*2
-    print(f"Number : {value}")
-
-
-print(f"List of numbers: {list_nums}")
+    # value=value*2
+    print(list_nums)
+# max number from the list
+max_num = max(list_nums)
+min_num= min(list_nums)
+sum_num = sum(list_nums)
+even_nums = list(range(0,100,2))
+print("Max number from the list:", max_num)
+#minimum number from the list
+print("Minimum number from the list:", min_num)
+print("Sum of all numbers in the list:", sum_num)
+print("Even numbers from 0 to 100:", even_nums)
