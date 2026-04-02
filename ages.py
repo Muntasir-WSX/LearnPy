7cadcb5a18548c83ed4824cf2d4b1381e0f64031
Muntasir <alimuntasir2001@gmail.com>
2026-04-02 13:34:50 +0600

    Python learning 2

---
+++ ages.py
@@ -0,0 +1,15 @@
+ages = [ 40,35,30,45,25,]
+print("Ages before sorting:", ages)
+ages.sort()
+print("Ages after sorting:", ages)
+
+
+
+furits=["banana", "apple", "grape", "orange", "kiwi"]
+print("Fruits before sorting:", furits)
+furits.sort()
+print("Fruits after sorting:", furits)
+
+list_len = len(ages)
+print ("Length of ages list:", list_len)
+
