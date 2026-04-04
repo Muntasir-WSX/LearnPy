86d0541a2c72a82f347d9f3bf57785044063b4a9
Muntasir <alimuntasir2001@gmail.com>
2026-04-04 21:50:13 +0600

    Python learning 11

---
+++ day.py
@@ -0,0 +1,13 @@
+day = "Monday"
+
+fridge_contents = ["milk", "eggs", "cheese"]
+
+
+if day == "Monday":
+    if "eggs" and "milk" in fridge_contents:
+        print("You have the ingredients for breakfast.")
+    else:
+        print("You need to buy some ingredients for breakfast.")
+        
+else:
+        print ("Stick to the quick breakfast options like cereal or toast.")
