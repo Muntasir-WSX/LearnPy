87a73c465c2a630e4fd159593ef16583b47dcc06
Muntasir <alimuntasir2001@gmail.com>
2026-04-04 21:40:58 +0600

    Python learning 10

---
+++ age.py
@@ -0,0 +1,9 @@
+age = 23
+toppings = ["pepperoni", "mushrooms", "olives"]
+if age != 23:
+    print("You are not 23 years old.")
+else:
+    print("You are exactly 23 years old.")
+    
+if "pepperoni" in toppings:
+    print("You like pepperoni.")
