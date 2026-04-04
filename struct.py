71357629e9027e14c22c37def52d7ce253a9b922
Muntasir <alimuntasir2001@gmail.com>
2026-04-04 22:01:57 +0600

    Python learning 12

---
+++ struct.py
@@ -0,0 +1,8 @@
+full_name = { "Paoulo Coelho": "The Alchemist", "J.K. Rowling": "Harry Potter"}
+print(full_name)
+
+person = {"name": "Alice", "age": 30, "city": "New York"}
+# print(person["name"])
+
+print (person.get("age"))
+
