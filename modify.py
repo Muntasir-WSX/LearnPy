1df7a067ab7e2bb13b4acec8b56bb9a473430b97
Muntasir <alimuntasir2001@gmail.com>
2026-04-05 11:05:43 +0600

    Python learning 13

---
+++ modify.py
@@ -0,0 +1,16 @@
+full_name = { "Paoulo Coelho": "The Alchemist", "J.K. Rowling": "Harry Potter"}
+
+person = {"name": "Alice", "age": 30, "city": "New York"}
+
+person["email"] = "alex@example.com"
+
+person ["is_Employed"] = True
+
+person ["age"] = 31
+
+# person.pop("age")
+# person.popitem()
+del person["is_Employed"]
+person.clear()
+
+print(person)
