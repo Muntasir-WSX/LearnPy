d46cd83b35a5ba6e7397a56167a7820099960bce
Muntasir <alimuntasir2001@gmail.com>
2026-04-06 00:25:19 +0600

    Python learning 18

---
+++ whileloops.py
@@ -0,0 +1,21 @@
+# counter = 0
+
+# while counter < 5:
+#  print(f"Counter is at: {counter}")
+ 
+#  counter += 1
+
+prompt = "\n Enter 'quit' to end the program. "
+prompt += "\n Enter your Command: "
+# print(prompt)
+
+while True:
+    command = input (prompt)
+    
+    if command == 'quit':
+        break
+else:
+ print(f"You have exited the program.")
+ 
+ 
+    
