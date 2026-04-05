95f04223cd8fa74f4903635a78afc727fc73be65
Muntasir <alimuntasir2001@gmail.com>
2026-04-05 13:41:52 +0600

    Python learning 16

---
+++ inputoutput.py
@@ -0,0 +1,7 @@
+name= input ("Enter your Name: ")
+age = input ("Enter your Age: ")
+# print(f"Your Name is: {name}")
+# print(f"Your Age is: {age}")
+mult = int(age) + 45
+
+print (f"Your Name is {name}, and you are {age} years old. In 45 years, you will be {mult} years old.")
