aaeb90271bd454465287393d8e6904233cbabe2e
Muntasir <alimuntasir2001@gmail.com>
2026-04-06 23:33:40 +0600

    python learning 26

---
+++ first.py
@@ -0,0 +1,6 @@
+def format_name(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+formatted_name =format_name(first_name="James", last_name="Bond")
+print(formatted_name)
