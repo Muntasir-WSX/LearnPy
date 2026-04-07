7241bd2f2359472960087adc7cb86db9d82dbab4
Muntasir <alimuntasir2001@gmail.com>
2026-04-07 23:00:42 +0600

    python learning 28

---
+++ plants.py
@@ -0,0 +1,9 @@
+
+plants=["rose", "tulip", "daisy", "sunflower"]
+
+def water_plants(plants):
+    
+    for plant in plants:
+        action=f"Watering {plant}."
+        print(action)
+water_plants(plants=plants)
