74b0e96c7ca7fdce2f8aa332939a616fba728e23
Muntasir <alimuntasir2001@gmail.com>
2026-04-07 23:08:39 +0600

    python learning 30

---
+++ toast.py
@@ -0,0 +1,10 @@
+import func
+
+result = func.add(5, 3)
+print(f"The sum of 5 and 3 is: {result}")
+result = func.subtract(10, 4)
+print(f"The difference between 10 and 4 is: {result}")
+result = func.multiply(6, 7)
+print(f"The product of 6 and 7 is: {result}")
+result = func.divide(20, 5)
+print(f"The quotient of 20 and 5 is: {result}")
