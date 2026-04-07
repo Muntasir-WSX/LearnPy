360d373deefbf9611147eddb2ecc9f82003e0a3b
Muntasir <alimuntasir2001@gmail.com>
2026-04-07 23:26:27 +0600

    python learning 31

---
+++ ger.py
@@ -0,0 +1,6 @@
+import func as math
+
+res = math.multiply(5, 3)
+res_tow = math.divide(10, 2)
+print(f"The result of multiplying 5 and 3 is: {res}")
+print(f"The result of dividing 10 by 2 is: {res_tow}")
